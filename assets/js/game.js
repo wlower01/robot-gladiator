61aae56efbcf7551d054d0c99c5a3c843d686d28
wlower01 <william.lower2477@gmail.com>
2021-10-29 01:30:09 -0500

    added beginning functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,28 @@
 var playerName = window.prompt("What is your robot's name?");
-    console.log(playerName);
+var playerHealth = 100;
+var playerAttack = 10;
 
-function fight() {
-    window.alert("The fight has begun!");
-}
+console.log(playerName, playerHealth. playerAttack);
 
-// fight(); 
+var enemyName = "Roberto";
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
+
+    /*Suntract the value of playerAttack from the value of
+    enemyHealth and use that result to update the value in the
+    enemy health variable*/
+    enemyHealth = enemyHealth - playerAttack
+    //log a resulting message to the console for confirmation
+    console.log(
+        playerName + " attacked " + enemyName + " . " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
+    //do step 1 again but for playerHealth and enemyAttack
+        playerHealth = playerHealth - enemyAttack;
+    //log another message
+    enemyName + " attacked " + playerName + "." + playerName + " now has " + playerHealth + " health remaining."
+  };
+
+fight();
