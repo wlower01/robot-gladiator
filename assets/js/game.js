0360ff1d8e2ed41c08874aa2b1b511fcc21eb812
wlower01 <william.lower2477@gmail.com>
2021-10-29 00:19:01 -0500

    console log added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,5 +1,8 @@
-window.alert("This is an alert! JavaScript is running!");
+var playerName = window.prompt("What is your robot's name?");
+    console.log(playerName);
 
 function fight() {
     window.alert("The fight has begun!");
 }
+
+// fight(); 
